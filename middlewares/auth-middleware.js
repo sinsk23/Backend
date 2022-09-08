7a1773822d2bcb4eea787ce7df18f9202ea7bfa7
Seongkyu <sinsk23@naver.com>
2022-09-08 11:22:05 +0900

    feat : middlewares 임시 미들웨어 추가, fix : comment,recomment 예외처리
    예외처리 작업중

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("../models");
+require("dotenv").config();
+
+module.exports = (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    const [tokenType, tokenValue] = authorization.split(" ");
+
+    if (tokenType !== "Bearer") {
+      res.status(401).json({
+        result: false,
+        errormessage: "토큰 타입이 맞지 않습니다.",
+      });
+      return;
+    }
+    try {
+      const privatekey = jwt.verify(tokenValue, process.env.MYSECRET_KEY);
+      // console.log(privatekey);
+      User.findByPk(privatekey.userId).then((userId, nickname) => {
+        res.locals = { userId, nickname };
+        next();
+      });
+    } catch (err) {
+      res.status(401).json({
+        result: false,
+        errormessage: "토큰 유효성 검사에 실패했습니다.",
+      });
+      return;
+    }
+  } catch (error) {
+    res.status(401).json({
+      result: false,
+      errormessage: "로그인 후 사용 가능합니다.",
+    });
+    return;
+  }
+};
