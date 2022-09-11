63ccd6b60a810a8c7e71b2a7bb1b1df17e127876
sinsk23 <sinsk23@naver.com>
2022-09-11 15:58:39 +0900

    fix : comment repo 주석처리

--- repositories/comments.repository.js
+++ repositories/comments.repository.js
@@ -14,20 +14,43 @@ class CommentRepository {
       userId,
     });
   };
-  //Repo 특정 게시글의 전체댓글 postId조회 //페이지네이션연습
+  //Repo 특정 게시글의 전체댓글 postId조회
+  /**
+   * @todo 유저와 연결 후 nickname,image,count 추가해주기
+   * 
+   * @param {integer}postId 게시글 아이디 번호 넣기 
+   * @param {number}pagenum 페이지 끊어 보여주기 ex)offset = 5 * (pagenum - 1);
+   * @param offset skip
+   * @param limit 댓글수 제한적 보여주기 
+   * @returns 게시글에 전체 댓글을 보여줌
+   */
   findinPostid = async (postId,pagenum) => {
     let offset = 0;
     if (pagenum > 1) {
       offset = 5 * (pagenum - 1);
     }
-    const inPostid = await Comment.findAll({
+    const inPostid = await Comment.findAndCountAll({
       where: { postId },
+      include:{model : User, attributes:["nickname","image"]},
       order: [["createdAt", "ASC"]],
       limit: 5,
       offset: offset,
     });
-    
+    // const commentData = inPostid.map((e)=>{
+    //   return{
+    //     commentId: e.commentId,
+    //     comment: e.comment,
+    //     createdAt: e.createdAt,
+    //     updatedAt: e.updatedAt,
+    //     postId: e.postId,
+    //     userId: e.User.userId,
+    //     nickname: e.User.nickname,
+    //     image: e.User.image
+        
+    //   }
+    // })
     return inPostid;
+    // return commentData;
   };
 
   //Repo 특정 게시글에 댓글 수정
@@ -52,19 +75,35 @@ class CommentRepository {
     if (pagenum > 1) {
       offset = 5 * (pagenum - 1);
     }
-    const inRecommentid = await ReComment.findAll({
+    const inRecommentid = await ReComment.findAndCountAll({
       where: { commentId },
-      // include:{model : User, attributes:["nickname","profile"]},
+      // include:{model : User, attributes:["nickname","image"]},
       order: [["createdAt", "ASC"]],
       limit: 5,
       offset: offset,
+      
     });
+    // const recommentData = inRecommentid.map((e)=>{
+    //   return{recommentId:e.recommentId,
+    //     comment: e.comment,
+    //     createdAt: e.createdAt,
+    //     updatedAt: e.updatedAt,
+    //     commentId: e.commentId,
+    //     userId: e.User.userId,
+    //     nickname: e.User.nickname,
+    //     image: e.User.image
+        
+    //   }
+    // })
     
     return inRecommentid;
+    // return recommentData;
+    
   };
   //Repo 특정 대댓글 수정
-  editRecomment = async (userId, commentId,recommentId, comment) => {
-    return await ReComment.update({ comment }, { where: { userId, commentId,recommentId } });
+  editRecomment = async (userId, commentId, recommentId, comment) => {
+    console.log("REPO : ",userId);
+    return await ReComment.update({ comment }, { where: { userId, commentId, recommentId } });
   };
   //Repo 특정 대댓글 삭제
   deleteRecomment = async (userId, commentId, recommentId) => {
--- routes/comments.js
+++ routes/comments.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const Comment = require("../controllers/comments.controller");
 const authMiddleware = require("../middlewares/auth-middleware");
 const CommentController = new Comment();
-//나중에 유저에서 include 찾아서 nickname이랑 profile 넣어줘야함
-//댓글 개수도 표현해줘야함 comment.length??or findandCountAll
+/**나중에 유저에서 include 찾아서 nickname이랑 이미지 image 넣어줘야함
+댓글 개수도 표현해줘야함 comment.length??or findandCountAll
+*/
 // /comment 루트로들어와
 
 // query로 받을때 주소는 "/?"
