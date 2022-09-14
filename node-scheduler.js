a888a7063a301f1af63847c265bf0ca7340d10cf
rmadbstjd <rmadbstjd@naver.com>
2022-09-14 15:54:55 +0900

    feat : 매주 월요일마다 기록 리셋 & 매일 00시 기준으로 기록 변경

--- node-scheduler.js
+++ node-scheduler.js
@@ -1,18 +1,39 @@
 const schedule = require("node-schedule");
 const { Record } = require("./models");
-class schedules {
-  set = (s) => {
-    console.log(s);
-    let rule = new schedule.RecurrenceRule();
-    rule.dayOfWeek = s.dayOfweek;
-    rule.hour = s.hour;
-    rule.minute = s.minute;
+let count = 0;
+let day = 0;
 
-    let job = schedule.scheduleJob(rule, function () {
-      console.log("Scheduler has executed!!!");
-      const deleteRecord = async () => await Record.destroy({ where: {} });
-      deleteRecord();
-    });
-  };
-}
-module.exports = schedules;
+let set1 = (s) => {
+  console.log(s);
+  let rule = new schedule.RecurrenceRule();
+  rule.dayOfWeek = s.dayOfweek;
+  rule.hour = s.hour;
+  rule.minute = s.minute;
+
+  let job = schedule.scheduleJob(rule, function () {
+    console.log("함수실행중1분마다", day);
+    //const deleteRecord = async () => await Record.destroy({ where: {} });
+    //deleteRecord();
+    count++;
+    day++;
+    if (day === 7) {
+      day = 0;
+    }
+    exports.day = day;
+  });
+};
+let set2 = (s) => {
+  let rule = new schedule.RecurrenceRule();
+  rule.dayOfWeek = s.dayOfweek;
+  rule.hour = s.hour;
+  rule.minute = s.minute;
+
+  let job = schedule.scheduleJob(rule, function () {
+    console.log("실행됐나?");
+    const deleteRecord = async () => await Record.destroy({ where: {} });
+    deleteRecord();
+  });
+};
+exports.day = day;
+exports.set1 = set1;
+exports.set2 = set2;
