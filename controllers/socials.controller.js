85b20c8969734bc5c360e26e1c9f1d7adcebef53
rmadbstjd <rmadbstjd@naver.com>
2022-09-16 02:01:08 +0900

    feat : 카카오 로그아웃 기능 구현

--- controllers/socials.controller.js
+++ controllers/socials.controller.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
 const { User } = require("../models");
+const axios = require("axios");
 class SocialController {
   kakaologin = (req, res, next) => {
     passport.authenticate(
@@ -10,8 +11,17 @@ class SocialController {
       (err, user, info) => {
         if (err) return next(err);
 
-        const { email, nickname, accessToken, image, provider } = user;
-
+        const { email, nickname, accessToken, refreshToken, image, provider } =
+          user;
+        console.log(
+          "악세스토큰",
+          email,
+          nickname,
+          accessToken,
+          refreshToken,
+          image,
+          provider
+        );
         const emailCheck = async (email) => {
           const emailCheck = await User.findOne({ where: { email } });
 
@@ -53,6 +63,22 @@ class SocialController {
         emailCheck(email);
       }
     )(req, res, next);
+  };
+  kakaologout = async (req, res, next) => {
+    const access_token =
+      "QpPQh_KPFb6Bgb26FcoRs6K6Z2bGy4iv7ZFkyGApCinJYAAAAYNCCg9f";
+    try {
+      const unlink = await axios({
+        //Promise 객체를 unlink에 넘겨주고
+        method: "POST",
+        url: "https://kapi.kakao.com/v1/user/unlink",
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
   };
   naverlogin = (req, res, next) => {
     passport.authenticate(
--- routes/socials.js
+++ routes/socials.js
@@ -10,6 +10,8 @@ const socialController = new SocialController();
 router.get("/kakao/login", passport.authenticate("kakao"));
 // GET: /api/kakao/callback 카카오 로그인 이후 콜백
 router.get("/kakao/callback", socialController.kakaologin);
+
+router.get("/kakao/logout", socialController.kakaologout);
 // GET: /api/naver/login 네이버 로그인
 router.get("/naver/login", passport.authenticate("naver"));
 // GET: /api/naver/callback 네이버 로그인 이후 콜백
