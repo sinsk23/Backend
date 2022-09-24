1653d300cb1c5b13bf30875f316e217ef6baa119
rmadbstjd <rmadbstjd@naver.com>
2022-09-24 15:47:54 +0900

    fix : Post 테이블의 speed,view column 삭제

--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -15,7 +15,6 @@ class PostRepository {
     time,
     distance,
     path,
-
     image,
     hashtag,
     userId,
@@ -27,7 +26,6 @@ class PostRepository {
       time,
       distance,
       path,
-
       image,
       hashtag,
       userId,
